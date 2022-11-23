1b5c469dc1f7706d84bc87413546ed7d60abf876
EliusCaleb <eliuswanyoro@gmail.com>
2022-11-23 15:30:23 +0300

    Learning

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  
   def index
     if params[:user_id]
       user = User.find(params[:user_id])
@@ -31,5 +32,5 @@ class ItemsController < ApplicationController
     params.permit(:name, :description, :price, :user_id)
   end
 
-  
+
 end
